67f355fa1a8adbcdd978120870633c0aa234f9d3
John Carlo Asilo <jcarloasilo@gmail.com>
2024-03-20 12:04:00 +0800

    add more shadcn components

--- src/lib/components/ui/form/form-errors.svelte
+++ src/lib/components/ui/form/form-errors.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	export let errors: string[] = [];
+	export let errors: string[] | undefined;
 </script>
 
 <div class="text-destructive" aria-live="assertive">
-	{#if errors.length}
+	{#if errors && errors.length}
 		{#each errors as err}
 			<p>{err}</p>
 		{/each}
