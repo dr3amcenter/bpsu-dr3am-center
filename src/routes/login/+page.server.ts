f065df8697c823960864b3523cb3643c87b3051d
John Carlo Asilo <jcarloasilo@gmail.com>
2024-03-20 12:05:29 +0800

    add login backend

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,11 +1,20 @@
-import { superValidate } from "sveltekit-superforms";
+import { setError, superValidate } from "sveltekit-superforms";
 import type { PageServerLoad, Actions } from "./$types";
 import { loginSchema } from "$lib/zod-schemas";
 
 import { zod } from "sveltekit-superforms/adapters";
-import { fail } from "@sveltejs/kit";
+import { fail, redirect } from "@sveltejs/kit";
 
-export const load: PageServerLoad = async () => {
+import { db } from "$lib/server/db";
+
+import { lucia } from "$lib/server/auth";
+import { Argon2id } from "oslo/password";
+
+export const load: PageServerLoad = async (event) => {
+	if (event.locals.user) {
+		redirect(303, `/${event.locals.user.role}/dashboard`);
+	}
+
 	return {
 		form: await superValidate(zod(loginSchema))
 	};
@@ -19,8 +28,34 @@ export const actions: Actions = {
 				form
 			});
 		}
-		return {
-			form
-		};
+
+		const { password, username } = form.data;
+
+		const existingUser = await db.query.userTable.findFirst({
+			where: (userTable, { eq }) => eq(userTable.username, username.toLowerCase())
+		});
+
+		if (!existingUser) {
+			return setError(form, "", "Incorrect username or password");
+		}
+
+		const validPassword = await new Argon2id().verify(existingUser.hashed_password, password);
+
+		if (!validPassword) {
+			return setError(form, "", "Incorrect username or password");
+		}
+
+		if (existingUser.isDeleted) {
+			return setError(form, "", "Account is inactive");
+		}
+
+		const session = await lucia.createSession(existingUser.id, {});
+		const sessionCookie = lucia.createSessionCookie(session.id);
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: ".",
+			...sessionCookie.attributes
+		});
+
+		redirect(302, `/${existingUser.role}/dashboard`);
 	}
 };
