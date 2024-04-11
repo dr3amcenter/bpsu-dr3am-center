43bd325c3aadf307c63ce260961e74a2dc17072e
John Carlo Asilo <jcarloasilo@gmail.com>
2024-04-11 20:33:09 +0800

    separate qr to diff components

--- src/lib/components/qr-code.svelte
+++ src/lib/components/qr-code.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
-	import QRCode, { type QRCodeRenderersOptions } from "qrcode";
 	import html2canvas from "html2canvas";
 
-	import { onMount } from "svelte";
-
 	import Dr3amLogo from "$lib/assets/img/dr3am-logo.png";
+	import Qr from "./qr.svelte";
 
 	export let value: string = "";
 	export let title: string = "";
-	export let options: QRCodeRenderersOptions = { width: 100, margin: 1 };
 
-	let canvas: HTMLCanvasElement;
 	let qrRef: HTMLElement;
 
 	function download() {
@@ -21,22 +17,15 @@
 			link.click();
 		});
 	}
-
-	onMount(async () => {
-		if (canvas) {
-			await QRCode.toCanvas(canvas, value, options);
-		}
-	});
 </script>
 
 <div class="flex flex-col items-start">
 	<div
-		data-sveltekit-noscroll
 		bind:this={qrRef}
 		class="relative inline-flex rounded-md border p-1.5 hover:cursor-pointer"
 		on:click={download}
 	>
-		<canvas bind:this={canvas} />
+		<Qr {value} />
 		<img
 			src={Dr3amLogo}
 			alt="BPSU Dr3am Center Logo"
